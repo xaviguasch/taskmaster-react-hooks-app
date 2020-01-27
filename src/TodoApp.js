38b256b55427e7d230bd9c95d7afa1467cb3934e
xaviguasch <xaviguasch@gmail.com>
2020-01-27 20:17:00 +0100

    cleaned up starting files

---
+++ src/TodoApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoApp = () => {
+  return (
+    <div className='container'>
+      <h1>The React Hooks ToDo Project</h1>
+    </div>
+  )
+}
+
+export default TodoApp
